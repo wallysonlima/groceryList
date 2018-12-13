9a641f904458630ea09e5e06b5e894ce4dc78659
wallysonlima <wallyson.n.a.lima@gmail.com>
2018-12-13 18:54:02 -0200

    Created some logic in mainActivity of Popup

--- app/src/main/java/wallyson/lima/grocerylist/Activities/MainActivity.java
+++ app/src/main/java/wallyson/lima/grocerylist/Activities/MainActivity.java
@@ -3,15 +3,23 @@ package wallyson.lima.grocerylist.Activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import wallyson.lima.grocerylist.R;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private EditText groceryItem;
+    private EditText quantity;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createPopupDialog();
             }
         });
     }
@@ -50,5 +59,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createPopupDialog() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup, null);
+        groceryItem = (EditText) view.findViewById(R.id.groceryItem);
+        quantity = (EditText)  view.findViewById(R.id.groceryQty);
+        saveButton = (Button) view.findViewById(R.id.saveButton);
+
+        dialogBuilder.setView(view);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Todo: Save to db
+                //Todo: Go to next screen
+
+                saveGroceryToDB(v);
+            }
+        });
+    }
+
+    private void saveGroceryToDB(View v) {
+
     }
 }
