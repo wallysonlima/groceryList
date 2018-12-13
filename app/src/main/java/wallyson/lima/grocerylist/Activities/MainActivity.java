4fc98efb85bd680e26f380c8a3f39a8048adc00c
wallysonlima <wallyson.n.a.lima@gmail.com>
2018-12-13 18:43:21 -0200

    create popup.xml

--- app/src/main/java/wallyson/lima/grocerylist/Activities/MainActivity.java
+++ app/src/main/java/wallyson/lima/grocerylist/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package wallyson.lima.grocerylist;
+package wallyson.lima.grocerylist.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import wallyson.lima.grocerylist.R;
 
 public class MainActivity extends AppCompatActivity {
 
