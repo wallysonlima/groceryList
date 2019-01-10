905d7a3f8e24f0c2d87890a32e290e718e587241
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-01-10 20:37:34 -0200

    Create the functionality edit items

--- app/src/main/java/wallyson/lima/grocerylist/UI/RecyclerViewAdapter.java
+++ app/src/main/java/wallyson/lima/grocerylist/UI/RecyclerViewAdapter.java
@@ -4,11 +4,13 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -96,11 +98,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public void onClick(View v) {
             switch(v.getId()) {
                 case R.id.editButton:
+                    int position = getAdapterPosition();
+                    Grocery grocery = groceryItems.get(position);
+                    editItem(grocery);
                     break;
 
                 case R.id.deleteButton:
-                    int position = getAdapterPosition();
-                    Grocery grocery = groceryItems.get(position);
+                    position = getAdapterPosition();
+                    grocery = groceryItems.get(position);
                     deleteItem(grocery.getId());
                     break;
             }
@@ -139,6 +144,43 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                     notifyItemRemoved(getAdapterPosition());
 
                     dialog.dismiss();
+                }
+            });
+        }
+
+        public void editItem(final Grocery grocery) {
+            alertDialogBuilder = new AlertDialog.Builder(context);
+
+            inflater = LayoutInflater.from(context);
+            View view =inflater.inflate(R.layout.popup, null);
+
+            final EditText groceryItem = view.findViewById(R.id.groceryItem);
+            final EditText quantity = view.findViewById(R.id.groceryQty);
+            final TextView title = view.findViewById(R.id.title);
+
+            title.setText("Edit Grocery");
+            Button saveButton = view.findViewById(R.id.saveButton);
+
+            alertDialogBuilder.setView(view);
+            dialog = alertDialogBuilder.create();
+            dialog.show();
+
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    DatabaseHandler db = new DatabaseHandler(context);
+
+                    // Update item
+                    grocery.setName(groceryItem.getText().toString());
+                    grocery.setQuantity(groceryItem.getText().toString());
+
+                    if ( !groceryItem.getText().toString().isEmpty()  && !quantity.getText().toString().isEmpty() ) {
+                        db.updateGrocery(grocery);
+                        notifyItemChanged(getAdapterPosition(), grocery);
+                        dialog.dismiss();
+                    } else {
+                        Snackbar.make(view, "Add Grocery and Quantity", Snackbar.LENGTH_LONG).show();
+                    }
                 }
             });
         }
